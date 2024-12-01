4a0ce93d3c13ecbca79b1d59a71656c15ae5d588
Dakjebbin <154586081+Dakjebbin@users.noreply.github.com>
2024-12-02 00:11:11 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
      <img className='w-6 lg:hidden'  src={menu} alt="" />
      
       {/* for mobile devices */}
-  <nav className='bg-red-500 w-80 absolute right-0 top-0 h-screen lg:hidden '>
+  <nav className='bg-red-500 w-80 absolute right-0 top-0 h-screen sm:hidden '>
     
 <ul className='ml-10 mt-10 h-60 flex flex-col justify-between'>
 <Link to="/">
