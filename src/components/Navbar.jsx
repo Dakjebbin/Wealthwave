db3687c5347a632d1154621b19f1971b58e5f820
DakJebbin <Jebbinp@gmail.com>
2024-12-10 00:27:53 -0800

    landing page done and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,29 @@
 import "../styles/navbar.css"
 import { Link } from 'react-router-dom'
 import { assets } from "../assets/assest"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 
 const Navbar = () => {
     const [isNavActive, setIsNavActive] = useState(false);
+    const mobileNavRef = useRef(null);
+
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (mobileNavRef.current && !mobileNavRef.current.contains(event.target)) {
+          setIsNavActive(false);
+        }
+      };
+  
+      // Add event listener
+      document.addEventListener("mousedown", handleClickOutside);
+  
+      // Clean up the event listener on component unmount
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, []);
 
     const handleToggle = () => {
       setIsNavActive(!isNavActive);
@@ -55,22 +72,22 @@ Login
 
         {/* My mobile view starts here */}  
 
-        <nav className={`mobile-view ${isNavActive ? "active" : ""}`}>
+        <nav  ref={mobileNavRef} className={`mobile-view ${isNavActive ? "active" : ""}`}>
 
           <div className="close-button">
             <img src={assets.close_btn} alt="" onClick={handleToggle} />
           </div>
           <ul className="navbar-mobile">
                 <li>
-            <Link to="/" id="home">Home</Link>
+            <Link to="/" id="home" onClick={handleToggle}>Home</Link>
           </li>
 
             <li>
-              <a href="#courses">Courses</a>
+              <a href="#courses" onClick={handleToggle}>Courses</a>
             </li>
 
             <li>
-              <a href="#about_us">About Us</a>
+              <a href="#about_us" onClick={handleToggle}>About Us</a>
             </li>
 
             <li>
